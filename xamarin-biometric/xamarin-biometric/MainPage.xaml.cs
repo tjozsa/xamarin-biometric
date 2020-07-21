2aa1440dfad99f72fd2e7b552e94ca171985a0bb
Tamas Jozsa <tjozsa@gmail.com>
2020-07-21 21:36:38 +0200

    Initial commit

---
+++ xamarin-biometric/xamarin-biometric/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Plugin.Fingerprint;
+using Plugin.Fingerprint.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xamarin_biometric
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void AuthButton_Clicked(object sender, EventArgs e)
+        {
+            bool isFingerprintAvailable = await CrossFingerprint.Current.IsAvailableAsync(false);
+            if (!isFingerprintAvailable)
+            {
+                await DisplayAlert("Error",
+                    "Biometric authentication is not available or is not configured.", "OK");
+                return;
+            }
+
+            AuthenticationRequestConfiguration conf =
+                new AuthenticationRequestConfiguration("Authentication",
+                "Authenticate access to your personal data");
+
+            var authResult = await CrossFingerprint.Current.AuthenticateAsync(conf);
+            if (authResult.Authenticated)
+            {
+                //Success  
+                await DisplayAlert("Success", "Authentication succeeded", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Error", "Authentication failed", "OK");
+            }
+        }
+    }
+}
